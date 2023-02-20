14be487ab1325e5aeb908846f8f579efeaad3c73
l1ttlelol <louis@cowlibob.co.uk>
2023-02-20 22:55:55 +0000

    Laying Down fundamentals
    Laid down the structure for how I will structure the code and set-up the functions for the player and enemy turn within a run state.

--- Collaborative rpg project/Program.cs
+++ Collaborative rpg project/Program.cs
@@ -6,7 +6,46 @@ namespace Collaborative_rpg_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("heeheeheehar");
+            //VARIABLES                 =========================================================
+            //System Variables
+            string TempInput;
+            bool IsGameRunning = true;
+
+            //Player Variables
+            float PlayerHP = 10;
+
+            //RUN LOOP                  =========================================================
+            while(IsGameRunning == true)
+            {
+                PlayerTurn();
+                EnemyTurn();
+            }
+
+            //SYSTEM FUNCTION           =========================================================
+
+            //COMMON FUNCTIONS          =========================================================
+
+            void NextText_Placeholder()
+            {
+                Console.WriteLine("Press ENTER to continue");
+                TempInput = Console.ReadLine();
+                Console.Clear();
+            }
+
+            //PLAYER + ENEMY FUNCTIONS  =========================================================
+
+            void PlayerTurn()
+            {
+                Console.WriteLine("It is the Players turn");
+                NextText_Placeholder();
+            }
+
+            void EnemyTurn()
+            {
+                Console.WriteLine("It is the Enemy's turn");
+                NextText_Placeholder();
+            }
+            //ACTION FUNCTIONS          =========================================================
         }
     }
 }
